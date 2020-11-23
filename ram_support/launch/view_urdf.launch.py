89d79c4fe62132eb96189ccc73ddbc04d6acc0d2
George Dwyer <george.dwyer@ucl.ac.uk>
2020-11-23 16:46:44 +0000

    Setup small test workcell with iiwa, gripper, cutting table and milling table

--- ram_support/launch/view_urdf.launch.py
+++ ram_support/launch/view_urdf.launch.py
@@ -1,16 +1,67 @@
-import launch
-import launch.actions
-import launch.substitutions
-import launch_ros.actions
+import os
+import yaml
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import xacro
+
+
+def load_file(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, 'r') as file:
+            return file.read()
+    except EnvironmentError: # parent of IOError, OSError *and* WindowsError where available
+        return None
+
+
+def load_yaml(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, 'r') as file:
+            return yaml.safe_load(file)
+    except EnvironmentError: # parent of IOError, OSError *and* WindowsError where available
+        return None
 
 
 def generate_launch_description():
-    return launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument(
-            'node_prefix',
-            default_value=[launch.substitutions.EnvironmentVariable('USER'), '_'],
-            description='Prefix for node names'),
-        launch_ros.actions.Node(
-            package='demo_nodes_cpp', executable='talker', output='screen',
-            name=[launch.substitutions.LaunchConfiguration('node_prefix'), 'talker']),
-    ])
+    # Component yaml files are grouped in separate namespaces
+    package_path = get_package_share_directory('ram_support')
+    absolute_file_path = os.path.join(package_path, 'urdf/iiwa_gripper.urdf.xacro')
+    doc = xacro.process_file(absolute_file_path).toprettyxml(indent='  ')
+
+    # print(doc)
+
+    robot_description = {'robot_description' : doc}
+    # RViz
+    rviz_config_file = get_package_share_directory('ram_support') + "/launch/view_urdf.rviz"
+    rviz_node = Node(package='rviz2',
+                     executable='rviz2',
+                     name='rviz2',
+                     output='log',
+                     arguments=['-d', rviz_config_file],
+                     parameters=[robot_description])
+
+    # Static TF
+    # static_tf = Node(package='tf2_ros',
+    #                  executable='static_transform_publisher',
+    #                  name='static_transform_publisher',
+    #                  output='log',
+    #                  arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'world', 'panda_link0'])
+    joint_state_publisher_gui = Node(package='joint_state_publisher_gui',
+                                     executable='joint_state_publisher_gui',
+                                     name='joint_state_publisher_gui',
+                                     parameters=[robot_description]
+                                     )
+    # Publish TF
+    robot_state_publisher = Node(package='robot_state_publisher',
+                                 executable='robot_state_publisher',
+                                 name='robot_state_publisher',
+                                 output='both',
+                                 parameters=[robot_description])
+
+    return LaunchDescription([robot_state_publisher, rviz_node, joint_state_publisher_gui])
