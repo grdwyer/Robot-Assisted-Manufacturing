f8bd70d3ef41a335635dc1c97be965a038c419bb
George Dwyer <george.dwyer@ucl.ac.uk>
2021-07-02 09:53:39 +0000

    Made the control launch file now need to complete the interface file which will run rviz, rqt and the planner. The planner will be selected as a launch argument

--- ram_motion_planning/launch/ram_interface.launch.py
+++ ram_motion_planning/launch/ram_interface.launch.py
@@ -3,7 +3,8 @@ import yaml
 from ament_index_python.packages import get_package_share_directory
 import xacro
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.conditions import IfCondition
 from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution, TextSubstitution
 from launch_ros.actions import Node
@@ -46,7 +47,9 @@ def load_xacro(package_name, file_path, mappings):
 
 def generate_launch_description():
     # Adding arguments
-    declared_arguments = [DeclareLaunchArgument("robot_ip", default_value="192.170.10.2",
+    declared_arguments = [DeclareLaunchArgument("planner", default_value="base",
+                                                description="The toolpath planner to run (e.g. base, ompl, servo"),
+                          DeclareLaunchArgument("robot_ip", default_value="192.170.10.2",
                                                 description="IP address of the kuka KONI"),
                           DeclareLaunchArgument("robot_port", default_value="30200",
                                                 description="Port used by the FRI (30200 - 30209"),
@@ -59,6 +62,7 @@ def generate_launch_description():
     robot_port = LaunchConfiguration("robot_port")
     manipulator = LaunchConfiguration("real_manipulator")
     rviz = LaunchConfiguration("rviz")
+    planner = LaunchConfiguration("planner")
 
     # Component yaml files are grouped in separate namespaces
     ######################
@@ -83,7 +87,7 @@ def generate_launch_description():
             " ",
         ]
     )
-    print(robot_description_content)
+
     robot_description = {"robot_description": robot_description_content}
 
     robot_description_semantic_config = load_file('ram_moveit_config', 'config/iiwa_workcell.srdf')
@@ -99,35 +103,7 @@ def generate_launch_description():
     ompl_planning_yaml = load_yaml('ram_moveit_config', 'config/ompl_planning.yaml')
     ompl_planning_pipeline_config['move_group'].update(ompl_planning_yaml)
 
-    # Trajectory Execution Functionality
-    controllers_yaml = load_yaml('ram_moveit_config', 'config/controllers.yaml')
-    moveit_controllers = {'moveit_simple_controller_manager': controllers_yaml,
-                          'moveit_controller_manager': 'moveit_simple_controller_manager/MoveItSimpleControllerManager'}
-
-    trajectory_execution = {'moveit_manage_controllers': True,
-                            'trajectory_execution.allowed_execution_duration_scaling': 1.2,
-                            'trajectory_execution.allowed_goal_duration_margin': 0.5,
-                            'trajectory_execution.allowed_start_tolerance': 0.01}
-
-    planning_scene_monitor_parameters = {"publish_planning_scene": True,
-                                         "publish_geometry_updates": True,
-                                         "publish_state_updates": True,
-                                         "publish_transforms_updates": True}
-
     nodes = []
-    # Start the actual move_group node/action server
-    move_group_node = Node(package='moveit_ros_move_group',
-                           executable='move_group',
-                           output='screen',
-                           parameters=[robot_description,
-                                       robot_description_semantic,
-                                       kinematics_yaml,
-                                       ompl_planning_pipeline_config,
-                                       trajectory_execution,
-                                       moveit_controllers,
-                                       planning_scene_monitor_parameters
-                                       ])
-    nodes.append(move_group_node)
 
     # RViz
     rviz_config_file = get_package_share_directory('ram_motion_planning') + "/launch/moveit_dev_setup.rviz"
@@ -142,71 +118,17 @@ def generate_launch_description():
                                  ompl_planning_pipeline_config,
                                  kinematics_yaml,
                                  ],
-                     condition=IfCondition(rviz)
                      )
     nodes.append(rviz_node)
 
-    # Publish TF
-    robot_state_publisher = Node(package='robot_state_publisher',
-                                 executable='robot_state_publisher',
-                                 name='robot_state_publisher',
-                                 output='both',
-                                 parameters=[robot_description])
-    nodes.append(robot_state_publisher)
+    rqt_node = Node(package='rqt',
+                    executable='rqt',
+                    name='rqt',
+                    output='own_log',
+                    )
+    nodes.append(rqt_node)
 
-    # Iiwa settings
-    iiwa_controller = os.path.join(
-        get_package_share_directory('ram_moveit_config'),
-        'config',
-        'ros_controllers.yaml'
-    )
-    ros2_control_node = Node(
-        package='controller_manager',
-        executable='ros2_control_node',
-        parameters=[robot_description, iiwa_controller],
-        prefix=['nice -n -20 '],
-        output={
-            'stdout': 'screen',
-            'stderr': 'screen',
-        }
-    )
-    nodes.append(ros2_control_node)
+    planner = IncludeLaunchDescription(PythonLaunchDescriptionSource(
+        get_package_share_directory('ram_motion_planning') + '/launch/toolpath_planner.py'))
 
-    load_controllers = []
-    for controller in ["iiwa_arm_controller", "joint_state_controller", "gripper_forward_command_controller_position"]:
-        load_controllers += [
-            ExecuteProcess(
-                cmd=["ros2 run controller_manager spawner.py {}".format(controller)],
-                shell=True,
-                output="screen",
-            )
-        ]
-    nodes += load_controllers
-
-    # Gripper control
-    sim_gripper_controller = Node(package="ram_gripper_control",
-                                  executable="sim_gripper_controller",
-                                  name="sim_gripper_controller",
-                                  output="screen"
-                                  )
-    nodes.append(sim_gripper_controller)
-
-    # medpor config file
-    stock_config_file = get_package_share_directory('ram_tooling_support') + "/config/medpor_configuration.yaml"
-    # print(stock_config_file)
-    stock_handler = Node(package='ram_tooling_support',
-                         executable='stock_handler',
-                         name='stock_handler',
-                         output='log',
-                         parameters=[stock_config_file]
-                         )
-    nodes.append(stock_handler)
-
-    toolpath_handler = Node(package='ram_tooling_support',
-                            executable='toolpath_handler',
-                            name='toolpath_handler',
-                            output='log',
-                            )
-    nodes.append(toolpath_handler)
-
-    return LaunchDescription(declared_arguments + nodes)
+    return LaunchDescription(declared_arguments + nodes + planner)
