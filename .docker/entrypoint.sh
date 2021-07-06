405d588ba9acbfec0e4e6266b0e88d24b9ced9fc
George Dwyer <george.dwyer@ucl.ac.uk>
2021-07-06 14:05:15 +0000

    Incorrect path to source ros in the entrypoint

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # setup ros environment
-source "/opt/ros/foxy.setup.bash"
+source "/opt/ros/foxy/setup.bash"
 soruce "/moveit_ws/install/setup.bash"
 source "/dev_ws/install/setup.bash"
 exec "$@"
