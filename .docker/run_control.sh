e4510a6202535b1803e00ce964e511bfdf70ae44
George Dwyer <george.dwyer@ucl.ac.uk>
2022-04-05 11:03:33 +0100

    Changed control launch script to run ram_control launch at start Moved kuka driver to being a dependency installed in the image build process.

--- .docker/run_control.sh
+++ .docker/run_control.sh
@@ -4,24 +4,11 @@ branch=$(git branch --show-current)
 
 echo -e "Starting up RAM control container for branch $branch\nWARNING this container will be deleted once left, use ctrl-p then ctrl-q to detach and leave running and docker cp to take files if necessary"
 
-docker run -dit \
+docker run -it \
     --rm \
     --workdir="/dev_ws" \
     --net=host \
     --cap-add SYS_NICE \
     --name ram_control \
-    gdwyer/ram:$branch-amd64
-
-mkdir /tmp/ram_control_startup
-cd /tmp/ram_control_startup
-
-git clone git@github.com:grdwyer/Kuka_iiwa_ROS2_Driver.git
-git clone -b foxy git@github.com:grdwyer/iiwa_fri_description.git
-
-docker cp Kuka_iiwa_ROS2_Driver ram_control:/dev_ws/src/
-docker cp iiwa_fri_description ram_control:/dev_ws/src/
-
-cd /tmp
-rm -rf /tmp/ram_control_startup
-
-docker attach ram_control
+    gdwyer/ram:$branch-amd64 \
+    ros2 launch ram_motion_planning ram_control.launch.py
